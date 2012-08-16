90443f80d04a26016e3cf83fdb08af5cbf6d1d50
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2012-08-16 23:08:29 +0200

    Cleanup

--- src/com/example/androidtest/MainActivity.java
+++ src/com/example/androidtest/MainActivity.java
@@ -1,14 +1,9 @@
 package com.example.androidtest;
 
 import android.app.Activity;
-import android.graphics.Canvas;
-import android.graphics.ColorFilter;
-import android.graphics.Rect;
-import android.graphics.drawable.ClipDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
@@ -18,14 +13,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView img = (ImageView) findViewById(R.id.imageView1);
-
-        Drawable drawable = getResources().getDrawable(R.drawable.test);
-        ClipDrawable clip = new ClipDrawable(drawable, Gravity.TOP, ClipDrawable.HORIZONTAL);
-        Rect bounds = new Rect(0, 0, 200, 200);
-        clip.setBounds(bounds);
-        clip.setLevel(10000);// max is 10000
-        img.setImageDrawable(drawable);
+//        ImageView img = (ImageView) findViewById(R.id.imageView1);
+//
+//        Drawable drawable = getResources().getDrawable(R.drawable.test);
+//        img.setImageDrawable(drawable);
+//        ClipDrawable clip = new ClipDrawable(drawable, Gravity.TOP, ClipDrawable.HORIZONTAL);
+//        Rect bounds = new Rect(0, 0, 200, 200);
+//        clip.setBounds(bounds);
+//        clip.setLevel(10000);// max is 10000
 //        img.setImageDrawable(clip);
         
 //        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.main);
@@ -42,39 +37,5 @@ public class MainActivity extends Activity {
     static void printf(String format, Object...args) {
     	String message = String.format(format, args);
     	Log.d("test", message);
-    }
-
-    
-    static class MyClipDrawable extends Drawable {
-    	final Drawable mDrawable;
-    	Rect bounds = new Rect(0, 0, 100, 100);
-
-    	MyClipDrawable(Drawable drawable) {
-    		this.mDrawable = drawable;
-    	}
-    	
-		@Override
-		public void draw(Canvas canvas) {
-			printf("*** drawin");
-			canvas.save();
-//			canvas.clipRect(0, 0, 100, 100);
-			this.mDrawable.draw(canvas);
-			canvas.restore();
-		}
-
-		@Override
-		public int getOpacity() {
-			return this.mDrawable.getOpacity();
-		}
-
-		@Override
-		public void setAlpha(int alpha) {
-			this.mDrawable.setAlpha(alpha);
-		}
-
-		@Override
-		public void setColorFilter(ColorFilter cf) {
-			this.mDrawable.setColorFilter(cf);
-		}
     }
 }
