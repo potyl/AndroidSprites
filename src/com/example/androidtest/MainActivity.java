c27c88de175ec335d988a0fbfbe4117b56901311
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2012-08-17 15:39:02 +0200

    MySurafaceView shows something [setWillNotDraw(false)]

--- src/com/example/androidtest/MainActivity.java
+++ src/com/example/androidtest/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
@@ -12,26 +15,69 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        ImageView img = (ImageView) findViewById(R.id.imageView1);
-//
-//        Drawable drawable = getResources().getDrawable(R.drawable.test);
-//        img.setImageDrawable(drawable);
-//        ClipDrawable clip = new ClipDrawable(drawable, Gravity.TOP, ClipDrawable.HORIZONTAL);
-//        Rect bounds = new Rect(0, 0, 200, 200);
-//        clip.setBounds(bounds);
-//        clip.setLevel(10000);// max is 10000
-//        img.setImageDrawable(clip);
+        Button button = (Button) findViewById(R.id.button);
         
-//        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.main);
-//        MySurafaceView surfaveView = new MySurafaceView(this);
-//        printf("MySurafaceView: %s", surfaveView.getClass().getCanonicalName());
-//        surfaveView.setDrawable(drawable);
-//        surfaveView.setVisibility(View.VISIBLE);
-//        viewGroup.addView(surfaveView, 10, 10);
+        final ImageView img = (ImageView) findViewById(R.id.imageView1);
 
-//        MySurafaceView surfaceView2 = (MySurafaceView) findViewById(R.id.surface);
-//        surfaceView2.setDrawable(drawable);
+        button.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Drawable drawable = getResources().getDrawable(R.drawable.budapest);
+				printf("draw: %s x %s", drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+				printf("img: %s x %s", img.getWidth(), img.getHeight());
+
+				drawable.setLevel(10000);
+				drawable.setBounds(0, 0, 50, 50);
+				img.setImageDrawable(drawable);
+
+//				ClipDrawable clip = new ClipDrawable(drawable, Gravity.TOP, ClipDrawable.HORIZONTAL);
+//				Rect bounds = new Rect(0, 0, 20, 20);
+//				clip.setBounds(bounds);
+//				clip.setLevel(10000);// max is 10000
+//				img.setVisibility(View.VISIBLE);
+//				img.setImageDrawable(clip);
+
+				img.refreshDrawableState();
+				img.invalidate();
+			}
+		});
+
+        button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Drawable drawable = getResources().getDrawable(R.drawable.budapest);
+
+				ViewGroup viewGroup = (ViewGroup) findViewById(R.id.main);
+		        MySurafaceView surfaceView = new MySurafaceView(MainActivity.this);
+		        surfaceView.setWillNotDraw(false);
+		        printf("MySurafaceView: %s", surfaceView.getClass().getCanonicalName());
+		        surfaceView.setDrawable(drawable);
+		        surfaceView.setVisibility(View.VISIBLE);
+		        viewGroup.addView(surfaceView, 10, 10);
+			}
+		});
+
+        MySurafaceView surfaceView = (MySurafaceView) findViewById(R.id.surface);
+		Drawable drawable = getResources().getDrawable(R.drawable.budapest);
+        surfaceView.setWillNotDraw(false);
+        surfaceView.setDrawable(drawable);
+//        surfaceView.refreshDrawableState();
+//        surfaceView.invalidateDrawable(drawable);
+//        surfaceView.invalidate();
+
+        button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Drawable drawable = getResources().getDrawable(R.drawable.budapest);
+
+				printf("Use existin surface");
+		        MySurafaceView surfaceView = (MySurafaceView) findViewById(R.id.surface);
+//		        surfaceView.setWillNotDraw(false);
+		        surfaceView.setDrawable(drawable);
+//		        surfaceView.refreshDrawableState();
+//		        surfaceView.invalidateDrawable(drawable);
+//		        surfaceView.invalidate();
+			}
+		});
+
     }
 
     static void printf(String format, Object...args) {
--- src/com/example/androidtest/MySurafaceView.java
+++ src/com/example/androidtest/MySurafaceView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Paint.Style;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -25,6 +24,8 @@ public class MySurafaceView extends SurfaceView {
 		super(context, attrs, defStyle);
 	}
 	
+
+	
 	void setDrawable(Drawable drawable) {
 		this.mDrawable = drawable;
 		this.refreshDrawableState();
@@ -35,18 +36,22 @@ public class MySurafaceView extends SurfaceView {
 	public void draw(Canvas canvas) {
 		printf("***** Drawing my surface");
 		canvas.save();
-		//canvas.clipRect(0, 0, 100, 100);
-//		this.mDrawable.setBounds(0, 0, 100, 100);
 		Paint paint = new Paint(); 
-		paint.setColor(Color.WHITE);
-		paint.setStyle(Style.FILL);
-		canvas.drawPaint(paint);
+
+//		paint.setColor(Color.WHITE);
+//		paint.setStyle(Style.FILL);
+//		canvas.drawPaint(paint);
 		
+		printf("canvas: %s x %s", getWidth(), getHeight());
+		this.mDrawable.setBounds(0, 0, 100, 100);
 		this.mDrawable.draw(canvas);
-		paint.setColor(Color.BLACK);
-		paint.setTextSize(20);
-		canvas.drawText("Emo", 10, 10, paint);
-		canvas.drawRGB(255, 0, 0);
+
+		paint.setColor(Color.WHITE);
+		paint.setTextSize(30);
+		canvas.drawText("Emo", 20, 60, paint);
+
+		canvas.clipRect(0, 0, 100, 100);
+
 		canvas.restore();
 	}
 
