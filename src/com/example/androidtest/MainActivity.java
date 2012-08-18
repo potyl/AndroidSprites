a97cee3f0e173615e8d385fa5d18c3a1ff4e0d6e
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2012-08-18 12:13:25 +0200

    Cleanup

--- src/com/example/androidtest/MainActivity.java
+++ src/com/example/androidtest/MainActivity.java
@@ -3,16 +3,11 @@ package com.example.androidtest;
 import android.app.Activity;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ClipDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -33,65 +28,14 @@ public class MainActivity extends Activity {
 				Resources res = getResources();
 				Drawable drawable = res.getDrawable(R.drawable.budapest);
 				Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
-				Bitmap clippedBitmap = Bitmap.createBitmap(bitmap, 0, 0, 100, 100);
+				int w = img.getWidth();
+				int h = img.getHeight();
+				printf("img: %s x %s", w, h);
+				Bitmap clippedBitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h);
 				BitmapDrawable bitmapDrawable = new BitmapDrawable(res, clippedBitmap);
 				img.setImageDrawable(bitmapDrawable);
 			}
 		});
-
-
-        if (button == null) button.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View v) {
-				Drawable drawable = getResources().getDrawable(R.drawable.budapest);
-				printf("draw: %s x %s", drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
-				printf("img: %s x %s", img.getWidth(), img.getHeight());
-
-
-				ClipDrawable clip = new ClipDrawable(drawable, Gravity.TOP, ClipDrawable.HORIZONTAL);
-				Rect bounds = new Rect(0, 0, img.getWidth(), img.getHeight());
-				clip.setBounds(bounds);
-				clip.setLevel(10000);// max is 10000
-//				img.setVisibility(View.VISIBLE);
-				img.setImageDrawable(clip);
-
-				img.refreshDrawableState();
-				img.invalidate();
-			}
-		});
-
-        if (button == null) button.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Drawable drawable = getResources().getDrawable(R.drawable.budapest);
-
-				ViewGroup viewGroup = (ViewGroup) findViewById(R.id.main);
-		        MySurafaceView surfaceView = new MySurafaceView(MainActivity.this);
-		        surfaceView.setWillNotDraw(false);
-		        printf("MySurafaceView: %s", surfaceView.getClass().getCanonicalName());
-		        surfaceView.setDrawable(drawable);
-		        surfaceView.setVisibility(View.VISIBLE);
-		        viewGroup.addView(surfaceView, 10, 10);
-			}
-		});
-
-//        MySurafaceView surfaceView = (MySurafaceView) findViewById(R.id.surface);
-//		Drawable drawable = getResources().getDrawable(R.drawable.budapest);
-//        surfaceView.setWillNotDraw(false);
-//        surfaceView.setDrawable(drawable);
-////        surfaceView.refreshDrawableState();
-////        surfaceView.invalidateDrawable(drawable);
-////        surfaceView.invalidate();
-//
-//        if (button != null) button.setOnClickListener(new View.OnClickListener() {
-//			public void onClick(View v) {
-//				Drawable drawable = getResources().getDrawable(R.drawable.budapest);
-//
-//				printf("Use existin surface");
-//		        MySurafaceView surfaceView = (MySurafaceView) findViewById(R.id.surface);
-//		        surfaceView.setDrawable(drawable);
-//			}
-//		});
-
     }
 
     static void printf(String format, Object...args) {
